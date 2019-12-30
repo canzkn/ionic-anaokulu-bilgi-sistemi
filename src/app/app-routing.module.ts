92cd5886a2be546a8178d87c729f283e7dc7d35f
canzkn <42238652+canzkn@users.noreply.github.com>
2019-12-30 06:34:41 +0300

    üyelik gerektiren sayfalar için yapı tekrar düzenlendi.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { PrivateGuard } from './services/guards/private.guard'
 import { PublicGuard } from './services/guards/public.guard'
+import { UserdataService } from './services/resolver/userdata.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', canActivate: [PublicGuard], loadChildren: './welcome/welcome.module#WelcomePageModule' },
   { path: 'login', canActivate: [PublicGuard], loadChildren: './login/login.module#LoginPageModule' },
   { path: 'signup', canActivate: [PublicGuard], loadChildren: './signup/signup.module#SignupPageModule' },
-  { path: 'dashboard/home', canActivate: [PrivateGuard], loadChildren: './home/home.module#HomePageModule' }
+  { 
+    path: 'dashboard', 
+    canActivate: [PrivateGuard], 
+    resolve: {userData: UserdataService},
+    children: [
+      { path: '', redirectTo: 'dashboard/home', pathMatch: 'full' },
+      { path: 'home', loadChildren: './home/home.module#HomePageModule'}
+    ]
+  }
 ];
 
 @NgModule({
