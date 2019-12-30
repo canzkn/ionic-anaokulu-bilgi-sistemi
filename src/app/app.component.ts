4d3d75acc46264560db49043c85d84dfb2daebc2
canzkn <42238652+canzkn@users.noreply.github.com>
2019-12-30 13:48:57 +0300

    anasayfa düzenlendi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,74 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, LoadingController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './services/auth/auth.service';
+
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  public studentDataDB = {
+    StudentID:'',
+    StudentName:'',
+    StudentBirthday:'',
+    StudentSize:'',
+    StudentKilo:'',
+    StudentPicture: '',
+    StudentClass:''
+  }
+
+  public appPages = [
+    {
+      title: 'Anasayfa',
+      url: '/dashboard/home',
+      icon: 'home'
+    },
+    {
+      title: 'Öğrenci Bilgileri',
+      url: '/dashboard/profile',
+      icon: 'happy'
+    },
+    {
+      title: 'Veli Bilgileri',
+      url: '/dashboard/parent',
+      icon: 'person'
+    },
+    {
+      title: 'Mesajlar',
+      url: '/dashboard/messages',
+      icon: 'mail'
+    },
+    {
+      title: 'Etkinlikler',
+      url: '/dashboard/activity',
+      icon: 'megaphone'
+    },
+    {
+      title: 'Gün Sonu Raporu',
+      url: '/dashboard/endofday',
+      icon: 'calendar'
+    },
+    {
+      title: 'Öğretmenlerim',
+      url: '/dashboard/teachers',
+      icon: 'contacts'
+    }
+  ];
   
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private loading: LoadingController,
+    private auth: AuthService,
+    private router: Router,
   ) {
     this.initializeApp();
   }
@@ -23,5 +78,33 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  async logout()
+  {
+    const loading = await this.loading.create({
+      message: 'Çıkış yapılıyor...',
+      duration: 1000
+    });
+
+    loading.present().then(() => {
+      this.auth.logout()
+    })
+  }
+
+  // public sayfa kontrolü
+  isWelcomePage(): boolean
+  {
+    return this.router.url === '/welcome'
+  }
+
+  isLoginPage(): boolean
+  {
+    return this.router.url === '/login'
+  }
+
+  isRegisterPage(): boolean
+  {
+    return this.router.url === '/signup'
   }
 }
