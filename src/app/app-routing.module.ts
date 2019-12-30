4189e326d9ca7d3abb688744584ceec2251b6ade
canzkn <42238652+canzkn@users.noreply.github.com>
2019-12-30 06:29:28 +0300

    guard yapısı eklendi.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { PrivateGuard } from './services/guards/private.guard'
+import { PublicGuard } from './services/guards/public.guard'
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'welcome',
-    pathMatch: 'full'
-  },
-  {
-    path: 'welcome',
-    loadChildren: './welcome/welcome.module#WelcomePageModule'
-  }
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', canActivate: [PublicGuard], loadChildren: './welcome/welcome.module#WelcomePageModule' },
+  { path: 'login', canActivate: [PublicGuard], loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'signup', canActivate: [PublicGuard], loadChildren: './signup/signup.module#SignupPageModule' },
+  { path: 'dashboard/home', canActivate: [PrivateGuard], loadChildren: './home/home.module#HomePageModule' }
 ];
 
 @NgModule({
@@ -19,4 +17,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastService } from '../services/toast/toast.service';
+import { AuthService } from '../services/auth/auth.service';
+import { StorageService } from '../services/storage/storage.service';
+import { ConstantService } from '../services/constant/constant.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  formData = {
+    StudentID : '',
+    StudentPassword : ''
+  }
+
+  row_data: any;
+
+  constructor(
+    private router: Router, 
+    private toastService: ToastService, 
+    private auth: AuthService,
+    private storageService: StorageService,
+    private constantService: ConstantService,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  // boşluk kontrolü
+  checkFields(): boolean
+  {
+    let StudentID = this.formData.StudentID;
+    let StudentPassword = this.formData.StudentPassword.trim();
+
+    // boş input kontrolü
+    if(StudentID === "" || StudentPassword === "")
+    {
+      this.toastService.error("Boş Alan Bırakmayınız");
+      return false;
+    }
+  
+    return true;
+  }
+
+  login()
+  {
+    if(this.checkFields())
+    {
+      this.auth.login(this.formData).subscribe(
+        (res: any) => {
+          if(res.message == 'LOGIN_SUCCESS')
+          {
+            this.storageService.setData(this.constantService.AUTH, res.data)
+            this.router.navigate(['dashboard/home']);
+          }
+          else
+          {
+            this.toastService.error('Kullanıcı adı veya Şifre yanlış!')
+          }
+        }
+      )
+    }
+  }
+
+}
